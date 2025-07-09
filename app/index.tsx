7d2659a61c548dd9f9b52416bc657374c70953db
Stolir <119765066+Stolir@users.noreply.github.com>
2025-07-09 21:53:13 +0300

    Initial setup and installations

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+ 
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl font-bold text-center font-quicksand-bold text-primary">
+        Welcome to this app!
+      </Text>
+    </View>
+  );
+}
