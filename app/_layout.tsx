4d53d3f5d801395e4e44d1c066607351a2ee8f7c
Stolir <119765066+Stolir@users.noreply.github.com>
2025-07-13 16:04:02 +0300

    Setup store/user authentication using zustand

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,29 @@
+import useAuthStore from '@/store/auth.store';
+import * as Sentry from '@sentry/react-native';
 import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
-import {SplashScreen, Stack} from "expo-router";
 import "./globals.css";
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://ab34f9f58dd2cd687977c8beb72d3468@o4509650304696320.ingest.de.sentry.io/4509650330648656',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
+  const { isLoading, fetchAuthenticatedUser } = useAuthStore();
+
   const [fontsLoaded, error] = useFonts({
     "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
     "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
@@ -17,5 +37,16 @@ export default function RootLayout() {
     if(fontsLoaded) SplashScreen.hideAsync();
   }, [fontsLoaded, error]);
 
-  return <Stack screenOptions={{headerShown: false}}/>;
-}
+  useEffect(() => {
+    fetchAuthenticatedUser()
+  }, [])
+
+  if(!fontsLoaded || isLoading) return null;
+
+  return( 
+    <Stack screenOptions={{headerShown: false}}>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false}}/>
+      <Stack.Screen name="(auth)" options={{ headerShown: false}}/>
+    </Stack> 
+  );
+});
